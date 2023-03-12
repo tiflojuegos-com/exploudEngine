d93dd784addeb86b020e581bc24791ccbd2e3fb5
Sandl Okino <sanslash332@yahoo.es>
2023-03-12 04:17:37 -0300

    updated sample project to work with monogame 3.8.1, and use .net 6.0 instead .net framework. used nulastudio.netBeauty to clear the output folder and put all libraries on to the libraries folder. doesnt exist a native .net method to do thath without headachews, like in .netFramework

--- sampleGame/Program.cs
+++ sampleGame/Program.cs
@@ -1,22 +1,3 @@
-﻿using System;
-
-namespace sampleGame
-{
-#if WINDOWS || LINUX
-    /// <summary>
-    /// The main class.
-    /// </summary>
-    public static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            using (var game = new Game1())
-                game.Run();
-        }
-    }
-#endif
-}
+﻿
+using var game = new sampleGame.Game1();
+game.Run();
